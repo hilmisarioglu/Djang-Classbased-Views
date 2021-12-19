0e45273b8861514e4fb6158891177baf12046b11
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-12-19 18:59:45 +0100

    custom form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,20 @@
 from django import  forms
 from django.forms import fields
 from .models import Student
+from django.core.exceptions import ValidationError 
 
 
 class StudentForm(forms.ModelForm):
+    # Burada ilk yazilan isin clean olacak
+    def clean_number(self):
+        number = self.cleaned_data['number']
+        if not(1<number<1000):
+            raise ValidationError('Student should be between 1 and 1000')
+        return number
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+
+        
     class Meta:
         model = Student
         fields = "__all__"
